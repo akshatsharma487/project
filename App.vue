72050a5e70e12adb78794634a3ca735de4cd0f48
akshatsharma487 <164846170+akshatsharma487@users.noreply.github.com>
2024-08-13 11:53:46 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Login v-if="!isLoggedIn" />
+    <Quiz v-else />
+  </div>
+</template>
+
+<script>
+import Login from "./components/Login.vue";
+import Quiz from "./components/Quiz.vue";
+
+export default {
+  components: { Login, Quiz },
+  data() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  created() {
+    // Check if user is logged in
+    this.isLoggedIn = !!sessionStorage.getItem("user");
+  },
+};
+</script>
